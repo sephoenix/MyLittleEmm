7f4315cb28c1a508f1a63608bb8c1070da113e3f
Carlos Parodi <se.phoenix@gmail.com>
2022-03-02 13:54:20 +0100

    page-routes updated

--- routes/page.routes.js
+++ routes/page.routes.js
@@ -6,58 +6,62 @@ const { isAuthenticated } = require('../middleware/jwt.middleware');
 const Diary = require('../models/Diary.model');
 const Page = require('../models/Page.model');
 
-router.post('/pages', (req, res, next) => {
-  const { date, type, whoWrites, babyWeight, babyHeight, photo, isPublic, diaryId, content } = req.body;
+router.post('/diaries/:diaryId/pages', (req, res, next) => {
+  const { diaryId } = req.params;
+  const { date, type, whoWrites, babyWeight, babyHeight, photo, isPublic, content } = req.body;
   Page.create({ date, type, whoWrites, babyWeight, babyHeight, photo, isPublic, diary: diaryId, content })
     .then(response => res.status(201).json(response))
     .catch(err => res.json(err));
 });
 
-router.get('/:diaryId/pages', async (req, res, next) => {
+router.get('/diaries/:diaryId/pages', async (req, res, next) => {
   const { diaryId } = req.params;
 
-  // get pages where diary is this diaryId
   try {
-    await Diary.find(diaryId);
-    const pages = await Page.find();
+    const pages = await Page.find({ diary: diaryId });
     res.status(201).json({ pages });
   } catch (e) {
     res.json(e);
   }
 });
 
-router.get('/:diaryId/pages/:pageId', (req, res, next) => {
+router.get('/diaries/:diaryId/pages/:pageId', (req, res, next) => {
   const { pageId } = req.params;
+  const { diaryId } = req.params;
+  console.log(req.params);
+
   if (!mongoose.Types.ObjectId.isValid(pageId)) {
     res.status(400).json({ message: 'This page doesnt exists' });
     return;
   }
-  Page.findById(pageId)
+  Page.find({ diary: diaryId, _id: pageId })
     .then(page => res.status(200).json(page))
     .catch(err => res.json(err));
 });
 
-router.put('/:diaryId/pages/:pageId', (req, res, next) => {
+router.put('/diaries/:diaryId/pages/:pageId', (req, res, next) => {
   const { pageId } = req.params;
+  const { diaryId } = req.params;
   if (!mongoose.Types.ObjectId.isValid(pageId)) {
     res.status(400).json({ message: 'This page doesnt exists' });
     return;
   }
-  Page.findByIdAndUpdate(pageId, req.body, { new: true })
+  Page.findOneAndUpdate({ diary: diaryId, _id: pageId }, req.body, { new: true })
     .then(updatedPage => res.json(updatedPage))
     .catch(err => res.json(err));
 });
 
-router.delete('/:diaryId/pages/:pageId', (req, res, next) => {
+router.delete('/diaries/:diaryId/pages/:pageId', (req, res, next) => {
   const { pageId } = req.params;
+  const { diaryId } = req.params;
+
   if (!mongoose.Types.ObjectId.isValid(pageId)) {
     res.status(400).json({ message: 'This page doesnt exists' });
     return;
   }
-  Page.findByIdAndDelete(pageId)
+  Page.findOneAndDelete({ diary: diaryId, _id: pageId })
     .then(() => res.json({ message: `Page with ${pageId} is removed successfully` }))
     .catch(err => res.json(err));
-  //TODO redirect
 });
 
 module.exports = router;
